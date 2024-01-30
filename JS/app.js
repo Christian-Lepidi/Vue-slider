17eb7a9361859a17e5f9a02260df4f9902ab7819
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-30 15:35:43 +0100

    feat: add wrong code

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,14 @@ const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    return {};
+    return {
+      images: [
+        "./vue-slider-markup-base/img/01.webp",
+        "./vue-slider-markup-base/img/02.webp",
+        "./vue-slider-markup-base/img/03.webp",
+        "./vue-slider-markup-base/img/04.webp",
+        "./vue-slider-markup-base/img/05.webp",
+      ],
+    };
   },
 }).mount("#app");
